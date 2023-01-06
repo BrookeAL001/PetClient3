7ec8ddf45333de2d66c2b859d05ca3015ecd6c3e
BrookeAL0012 <bbcecil17@gmail.com>
2023-01-06 14:33:40 -0600

    Add project files.

---
+++ PetClient3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetClient3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
